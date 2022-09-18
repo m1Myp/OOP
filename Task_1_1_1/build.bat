5840add696b1eff9c8731770330a72837c222edf
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-09-18 19:00:51 +0700

    Add files via upload

--- Task_1_1_1/build.bat
+++ Task_1_1_1/build.bat
@@ -3,4 +3,5 @@ javadoc -d .\batch\doc\ -charset utf-8 -sourcepath .\src\main\java\ -subpackages
 javac -sourcepath .\src\main\java\ -d .\batch\bin\ .\src\main\java\ru\nsu\fit\oop\lab1\HeapSort.java
 
 mkdir .\batch\jar\
+
 jar cf .\batch\jar\HeapSort.jar -C .\batch\bin\ .
