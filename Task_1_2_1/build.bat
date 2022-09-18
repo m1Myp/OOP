a7a6d246f67be682f31f2787171068796ca2a49f
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-09-18 19:29:52 +0700

    Update build.bat

--- Task_1_2_1/build.bat
+++ Task_1_2_1/build.bat
@@ -3,4 +3,4 @@ javadoc -d .\batch\doc\ -charset utf-8 -sourcepath .\src\main\java\ -subpackages
 javac -sourcepath .\src\main\java\ -d .\batch\bin\ .\src\main\java\ru\nsu\fit\oop\lab1\Stack.java
 
 mkdir .\batch\jar\
-jar cf .\batch\jar\HeapSort.jar -C .\batch\bin\ .
+jar cf .\batch\jar\Stack.jar -C .\batch\bin\ .
