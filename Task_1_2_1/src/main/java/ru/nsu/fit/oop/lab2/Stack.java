12374e1d0408054eef9b44d8b48c62f7e0a8b962
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-09-18 19:22:45 +0700

    Add files via upload

---
+++ Task_1_2_1/src/main/java/ru/nsu/fit/oop/lab2/Stack.java
@@ -0,0 +1,99 @@
+package ru.nsu.fit.oop.lab2;
+
+import java.lang.reflect.Array;
+import java.util.EmptyStackException;
+
+
+public class Stack <T>{
+
+    private T[] arr;
+    private int id;
+    private final Class<T> stackClass;
+
+
+    @SuppressWarnings("unchecked")
+    public Stack (int maxSize, Class<T> type)
+    {
+        stackClass = type;
+        arr = (T[]) Array.newInstance(stackClass, maxSize);
+        id = 0;
+    }
+
+    /**
+     * size getter
+     * @return Stack.AmountOfElems
+     */
+    public int getAmount()
+    {
+        return id;
+    }
+    @SuppressWarnings("unchecked")
+    private void realloc()
+    {
+        T[] newArray = (T[]) Array.newInstance(stackClass, arr.length * 2);
+        System.arraycopy(arr, 0, newArray, 0, id);
+        arr = newArray;
+    }
+
+    /**
+     * adding elem to the Stack and realloc array if needed
+     * @param elem
+     */
+    public void push(T elem)
+    {
+        if (id == arr.length)
+            realloc();
+
+        arr[id++] = elem;
+
+    }
+
+    /**
+     * method for popping from stack.
+     * @return the last added elem from the stack
+     * @throws EmptyStackException if you'll try to pop from empty stack.
+     */
+    public T pop() throws EmptyStackException
+    {
+        if (id == 0)
+            throw new EmptyStackException();
+        else
+            return arr[--id];
+    }
+
+    /**
+     * method for popping from stack.
+     * @return the n last element from stack like type stack
+     * @throws EmptyStackException if you'll try to pop from empty stack or if count of pushing stack more than existing one
+     */
+    public Stack<T> popStack(int count) throws EmptyStackException
+    {
+        Stack <T> returnStack = new Stack<>(count, stackClass);
+        if (id == 0 || id < count)
+            throw new EmptyStackException();
+        else {
+
+            for(int i = 0; i < count; ++i){
+                returnStack.push(arr[id-1]);
+                arr[id-1] = null;
+                --id;
+            }
+        }
+        return returnStack;
+    }
+
+    /**
+     * adding elem from the one Stack to another Stack and realloc array if needed
+     * @param pushingStack
+     */
+    public void pushStack(Stack<T> pushingStack)
+    {
+        int numOfElemsInPushingStack = pushingStack.id;
+        for(int i = 0; i < numOfElemsInPushingStack; ++i) {
+            if (id == arr.length)
+                realloc();
+
+            arr[id++] = pushingStack.arr[i];
+        }
+    }
+}
