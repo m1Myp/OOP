f5627af6fc76296f62e46d6a9440fed02c09cbc2
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-09-19 16:48:19 +0700

    Update HeapSortTests.java

--- Task_1_1_1/src/test/java/ru/nsu/fit/oop/lab1/HeapSortTests.java
+++ Task_1_1_1/src/test/java/ru/nsu/fit/oop/lab1/HeapSortTests.java
@@ -1,32 +1,57 @@
 package ru.nsu.fit.oop.lab1;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
-
-
-    public class HeapSortTests {
-        @ParameterizedTest
-        @MethodSource ("allTests")
-        public void simpleTests(int[] arr) {
-           int[] sortedArr = arr.clone();
-            Arrays.sort(sortedArr);
-            HeapSort.heapSort(arr);
-            assertArrayEquals(sortedArr, arr);
-        }
-
-        private static Stream<int[]> allTests() {
-            return Stream.of(
-                    new int[]{4, 5, 1, 0, -5},
-                    new int[]{99, 23, -38, -93, 0, 0, -67, 8, 39, -34},
-                    new int[]{3, 1, 2, Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE},
-                    new int[]{},
-                    new int[]{1}
-            );
-        }
+/**
+ * HeapSort testing class.
+ */
+public class HeapSortTests {
+    
+    /**
+     * Test with random numbers.
+     */
+    @ParameterizedTest
+    @MethodSource ("allTests")
+    public void simpleTests(int[] arr) {
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+        HeapSort.heapSort(arr);
+        assertArrayEquals(sortedArr, arr);
     }
+    
+    /**
+     * Test with empty array.
+     */
+    @Test
+    public void testEmptyArr() {
+        int[] emptyArr = {};
+        int[] emptyArrSorted = {};
+        HeapSort.heapSort(emptyArr);
+        assertArrayEquals(emptyArr, emptyArrSorted);
+    }
+    
+    /**
+     * Test array with one elements.
+     */
+    @Test
+    public void testOneElements() {
+        int[] OneElementsArr = {1};
+        int[] OneElementsArrSorted = {1};
+        HeapSort.heapSort(OneElementsArr);
+        assertArrayEquals(OneElementsArr, OneElementsArrSorted);
+    }
+
+    private static Stream<int[]> allTests() {
+        return Stream.of(
+            new int[]{4, 5, 1, 0, -5},
+            new int[]{99, 23, -38, -93, 0, 0, -67, 8, 39, -34},
+            new int[]{3, 1, 2, Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE}
+            );
+    }
+}
