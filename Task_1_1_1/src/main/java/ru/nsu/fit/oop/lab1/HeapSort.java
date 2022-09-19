a5bd4a5cd0ac2b6cbf7cdc32863362e25eca2941
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-09-19 16:40:23 +0700

    Исправляю варнинги

--- Task_1_1_1/src/main/java/ru/nsu/fit/oop/lab1/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/fit/oop/lab1/HeapSort.java
@@ -1,14 +1,17 @@
 package ru.nsu.fit.oop.lab1;
 
-
 import java.util.Arrays;
 
+/**
+ * HeapSort class.
+ */
 public class HeapSort {
     /**
-     * Swap two elements
+     * Swap two elements.
      *
-     * @param arr - array where elements swaps
-     * @param x,y - id of this elements
+     * @param arr array where elements swaps
+     * @param x 1st part of id of this elements
+     * @param y 2nd part of id of this elements
      */
     private static void swap(int[] arr, int x, int y) {
         int t = arr[x];
@@ -18,32 +21,34 @@ public class HeapSort {
 
     /**
      * Compares current id's element with his two children
-     * if parent greater then children swap them. Repeats this action before end of heap
+     * if parent greater then children swap them. Repeats this action before end of heap.
      *
      * @param arr heap
-     * @param N   len of heap
+     * @param len length of heap
      * @param id  index of element we should sift down
      */
-    private static void siftDown(int[] arr, int id, int N) {
+    private static void siftDown(int[] arr, int id, int len) {
         int current = id;
         int left = id * 2 + 1;
         int right = id * 2 + 2;
-        if (left < N && arr[current] > arr[left])
+        if (left < len && arr[current] > arr[left]) {
             current = left;
-        if (right < N && arr[current] > arr[right])
+        }
+        if (right < len && arr[current] > arr[right]) {
             current = right;
+        }
         if (current != id) {
             swap(arr, current, id);
-            siftDown(arr, current, N);
+            siftDown(arr, current, len);
         }
     }
 
     /**
-     * Find root of the tree (heap) preserving the heap invariant
-     * Swap with elem in last pos
+     * Find root of the tree (heap) preserving the heap invariant.
+     * Swap with elem in last pos.
      *
      * @param arr heap
-     * @param len len of heap
+     * @param len length of heap
      * @return the largest element of the heap (root of the tree)
      */
     private static void takeRoot(int[] arr, int len) {
@@ -53,21 +58,21 @@ public class HeapSort {
     }
 
     /**
-     * Heap sort of array
+     * Heap sort of array.
      *
      * @param arr array to sort
      */
     public static void heapSort(int[] arr) {
         int len = arr.length;
-        for (int i = len-1; i >= 0; i--)
+        for (int i = len - 1; i >= 0; i--) {
             siftDown(arr, i, len);
-
-        while(len > 0) {
+        }
+        while (len > 0) {
             takeRoot(arr, len);
             --len;
         }
-        for(int j = 0; j < arr.length/2; ++j){
-            swap(arr, j, (arr.length-1)-j);
+        for (int j = 0; j < arr.length / 2; ++j) {
+            swap(arr, j, (arr.length - 1) - j);
         }
     }
 }
