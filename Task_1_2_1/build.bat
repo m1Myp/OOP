410074f010bf7a63cd0f3893e05625e9e7d02886
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-09-21 17:57:09 +0700

    Delete build.bat

--- Task_1_2_1/build.bat
+++
@@ -1,6 +0,0 @@
-javadoc -d .\batch\doc\ -charset utf-8 -sourcepath .\src\main\java\ -subpackages ru.nsu.fit.oop.lab2
-
-javac -sourcepath .\src\main\java\ -d .\batch\bin\ .\src\main\java\ru\nsu\fit\oop\lab1\Stack.java
-
-mkdir .\batch\jar\
-jar cf .\batch\jar\HeapSort.jar -C .\batch\bin\ .
