56cb0da77cbf5fe7f7e14774e4ea9b57fab46a5b
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-09-21 10:35:38 +0700

    Add files via upload

---
+++ Task_1_3_1/src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
@@ -0,0 +1,59 @@
+package ru.nsu.fit.oop.lab2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+
+/**
+ * Class of the substring search.
+ */
+public class SubstringSearch {
+
+    /**
+     * Finds the indices of the occurrence of a substring {@param searchParam} in {@param file}.
+     *
+     * @param file Reader where we want to search occurrence
+     * @param str  String we want to find
+     * @return array of indices of the occurrences. Count from 0
+     * @throws IOException if some problems with a reading of the file
+     */
+    public static ArrayList<Integer>
+    finiteStateMashine(Reader file, String str) throws IOException {
+        if (file == null) {
+            throw new NullPointerException("File is null");
+        }
+        if (str == null) {
+            throw new NullPointerException("Searched string is null");
+        }
+
+        int id = 0;
+        ArrayList<Integer> indexes = new ArrayList<>();
+        boolean eofFlag = false;
+        try (BufferedReader br = new BufferedReader(file)) {
+            String buffArray = "";
+            for (int i = 0; i < str.length(); ++i) {
+                int c;
+                if ((c = br.read()) == -1) {
+                    eofFlag = true;
+                    break;
+                }
+                buffArray += (char) c;
+            }
+            while (!eofFlag) {
+                if (str.equals(buffArray)) {
+                    indexes.add(id);
+                }
+                char[] tempChars = new char[str.length() - 1];
+                buffArray.getChars(1, str.length(), tempChars, 0);
+                buffArray = String.valueOf(tempChars);
+                ++id;
+                int c;
+                if ((c = br.read()) == -1)
+                    eofFlag = true;
+                buffArray += (char) c;
+            }
+            return indexes;
+        }
+    }
+}
