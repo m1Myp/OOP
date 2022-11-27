bf2515c38f5d6ed8c4b38c9245108a928c2c3100
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-11-27 22:10:54 +0700

    Update SubstringSearch.java

--- Task_1_3_1/src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
@@ -1,9 +1,13 @@
-package ru.nsu.fit.oop.lab2;
+package ru.nsu.fit.oop.lab3;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.max;
+
 
 /**
  * Class of the substring search.
@@ -11,26 +15,70 @@ import java.util.ArrayList;
 public class SubstringSearch {
 
     /**
+     * Finds array of length of the longest native substring prefix
+     *
+     * @param str array of char we want to finds prefixes
+     * @param len len of array of char we want to find
+     * @return array of length of the longest native substring prefix
+     */
+    private static int[] prefixFunction(String str, int len){
+        int[] values = new int[str.length()];
+        for(int i = 0; i < len; ++i) values[i] = 0;
+        for(int i = len; i < str.length(); ++i){
+            int j = 0;
+            while(i + j < str.length() && str.charAt(j) == str.charAt(i+j)){
+                values[i+j] = max(values[i+j], j + 1);
+                ++j;
+            }
+        }
+        return values;
+    }
+
+    /**
      * Finds the indices of the occurrence of a substring {@param searchParam} in {@param file}.
      *
-     * @param file Reader where we want to search occurrence
-     * @param str  String we want to find
+     * @param reader Reader where we want to search occurrence
+     * @param str    String we want to find
      * @return array of indices of the occurrences. Count from 0
-     * @throws IOException if some problems with a reading of the file
+     * @throws IOException if some problems with a reading of the reader
      */
-    public static ArrayList<Integer>
-    finiteStateMashine(Reader file, String str) throws IOException {
-        if (file == null) {
+    public static List<Integer> zFunction(Reader reader, String str) throws IOException {
+
+        if (reader == null) {
             throw new NullPointerException("File is null");
         }
+
         if (str == null) {
             throw new NullPointerException("Searched string is null");
         }
 
+        List<Integer> indexes = new ArrayList<>();
+        String text = "";
+        try (BufferedReader br = new BufferedReader(reader)) {
+            int c;
+            while((c=br.read()) !=-1){
+                text += (char)c;
+            }
+        }
+        int[] prefixFunc = prefixFunction(str + '#' + text, str.length());
+
+        for (int i = 0; i <= text.length(); ++i) {
+            if (prefixFunc[i + str.length()] >= str.length())
+                indexes.add(i - str.length());
+        }
+
+        return indexes;
+    }
+
+}
+
+
+        /*
         int id = 0;
-        ArrayList<Integer> indexes = new ArrayList<>();
+        List<Integer> indexes = new ArrayList<>();
         boolean eofFlag = false;
-        try (BufferedReader br = new BufferedReader(file)) {
+
+        try (BufferedReader br = new BufferedReader(reader)) {
             String buffArray = "";
             for (int i = 0; i < str.length(); ++i) {
                 int c;
@@ -41,6 +89,8 @@ public class SubstringSearch {
                 buffArray += (char) c;
             }
             while (!eofFlag) {
+
+
                 if (str.equals(buffArray)) {
                     indexes.add(id);
                 }
@@ -52,8 +102,9 @@ public class SubstringSearch {
                 if ((c = br.read()) == -1)
                     eofFlag = true;
                 buffArray += (char) c;
+
             }
             return indexes;
         }
     }
-}
+} */
