63b1816ec6458698a05e2cccfad074dffe8aacf4
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-11-28 18:12:09 +0700

    Update SubstringSearch.java

--- Task_1_3_1/src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
@@ -1,110 +1,71 @@
-package ru.nsu.fit.oop.lab2;
+package ru.nsu.fit.oop.lab3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import static java.lang.Integer.max;
-
 
 /**
  * Class of the substring search.
  */
 public class SubstringSearch {
+/**
+ * Finds the indices of the occurrence of a substring {@param searchParam} in {@param file}.
+ *
+ * @param reader Reader where we want to search occurrence
+ * @param needle String we want to find
+ * @return array of indices of the occurrences. Count from 0
+ * @throws IOException if some problems with a reading of the reader
+ */
+    public static List<Integer> zFunction(Reader reader, String needle) throws IOException {
+        var rdr = new BufferedReader(reader);
 
-    /**
-     * Finds array of length of the longest native substring prefix
-     *
-     * @param str array of char we want to finds prefixes
-     * @param len len of array of char we want to find
-     * @return array of length of the longest native substring prefix
-     */
-    private static int[] prefixFunction(String str, int len){
-        int[] values = new int[str.length()];
-        for(int i = 0; i < len; ++i) values[i] = 0;
-        for(int i = len; i < str.length(); ++i){
-            int j = 0;
-            while(i + j < str.length() && str.charAt(j) == str.charAt(i+j)){
-                values[i+j] = max(values[i+j], j + 1);
-                ++j;
+        var m = needle.length();
+        var s = needle.toCharArray();
+        var suffshift = new int[m + 1];
+        Arrays.fill(suffshift, m);
+        var z = new int[m];
+        int maxZidx = 0;
+        int maxZ = 0;
+        for (int j = 1; j < m; j++) {
+            if (j <= maxZ) z[j] = Math.min(maxZ - j + 1, z[j - maxZidx]);
+            while (j + z[j] < m && s[m - 1 - z[j]] == s[m - 1 - (j + z[j])]) z[j]++;
+            if (j + z[j] - 1 > maxZ) {
+                maxZidx = j;
+                maxZ = j + z[j] - 1;
             }
         }
-        return values;
-    }
-
-    /**
-     * Finds the indices of the occurrence of a substring {@param searchParam} in {@param file}.
-     *
-     * @param reader Reader where we want to search occurrence
-     * @param str    String we want to find
-     * @return array of indices of the occurrences. Count from 0
-     * @throws IOException if some problems with a reading of the reader
-     */
-    public static List<Integer> zFunction(Reader reader, String str) throws IOException {
-
-        if (reader == null) {
-            throw new NullPointerException("File is null");
-        }
-
-        if (str == null) {
-            throw new NullPointerException("Searched string is null");
-        }
-
-        List<Integer> indexes = new ArrayList<>();
-        String text = "";
-        try (BufferedReader br = new BufferedReader(reader)) {
-            int c;
-            while((c=br.read()) !=-1){
-                text += (char)c;
+        for (int j = m - 1; j > 0; j--) suffshift[m - z[j]] = j;
+        for (int j = 1, r = 0; j <= m - 1; j++) {
+            if (j + z[j] == m) {
+                for (; r <= j; r++) {
+                    if (suffshift[r] == m) suffshift[r] = j;
+                }
             }
         }
-        int[] prefixFunc = prefixFunction(str + '#' + text, str.length());
 
-        for (int i = 0; i <= text.length(); ++i) {
-            if (prefixFunc[i + str.length()] >= str.length())
-                indexes.add(i - str.length());
+        int j, bound = 0;
+        int i = 0;
+
+        var cbuf = new char[m];
+        var ret = new ArrayList<Integer>();
+
+        while (true) {
+            rdr.mark(2 * m);
+            if (rdr.read(cbuf, 0, m) != m) break;
+            for (j = m - 1; j >= bound && s[j] == cbuf[j]; ) j--;
+            if (j < bound) {
+                ret.add(i);
+                bound = m - suffshift[0];
+                j = -1;
+            } else {
+                bound = 0;
+            }
+            rdr.reset();
+            if (rdr.skip(suffshift[j + 1]) != suffshift[j + 1]) break;
+            i += suffshift[j + 1];
         }
 
-        return indexes;
+        return ret;
     }
-
 }
-
-
-        /*
-        int id = 0;
-        List<Integer> indexes = new ArrayList<>();
-        boolean eofFlag = false;
-
-        try (BufferedReader br = new BufferedReader(reader)) {
-            String buffArray = "";
-            for (int i = 0; i < str.length(); ++i) {
-                int c;
-                if ((c = br.read()) == -1) {
-                    eofFlag = true;
-                    break;
-                }
-                buffArray += (char) c;
-            }
-            while (!eofFlag) {
-
-
-                if (str.equals(buffArray)) {
-                    indexes.add(id);
-                }
-                char[] tempChars = new char[str.length() - 1];
-                buffArray.getChars(1, str.length(), tempChars, 0);
-                buffArray = String.valueOf(tempChars);
-                ++id;
-                int c;
-                if ((c = br.read()) == -1)
-                    eofFlag = true;
-                buffArray += (char) c;
-
-            }
-            return indexes;
-        }
-    }
-} */
