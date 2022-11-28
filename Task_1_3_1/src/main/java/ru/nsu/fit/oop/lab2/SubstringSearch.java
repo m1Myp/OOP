d53a23c4655b640585fe206634258e9d55b2cafe
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-11-28 18:46:52 +0700

    Update SubstringSearch.java

--- Task_1_3_1/src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
@@ -1,6 +1,8 @@
 package ru.nsu.fit.oop.lab2;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,15 +11,15 @@ import java.util.List;
  * Class of the substring search.
  */
 public class SubstringSearch {
-/**
- * Finds the indices of the occurrence of a substring needle in reader.
- *
- * @param reader Reader where we want to search occurrence
- * @param needle String we want to find
- * @return array of indices of the occurrences. Count from 0
- * @throws IOException if some problems with a reading of the reader
- */
-    public static List<Integer> zFunction(Reader reader, String needle) throws IOException {
+    /**
+     * Finds the indices of the occurrence of a substring needle in reader.
+     *
+     * @param reader Reader where we want to search occurrence
+     * @param needle String we want to find
+     * @return array of indices of the occurrences. Count from 0
+     * @throws IOException if some problems with a reading of the reader
+     */
+    public static List<Integer> zfunction(Reader reader, String needle) throws IOException {
         var rdr = new BufferedReader(reader);
 
         var m = needle.length();
@@ -28,23 +30,32 @@ public class SubstringSearch {
         int maxZidx = 0;
         int maxZ = 0;
         for (int j = 1; j < m; j++) {
-            if (j <= maxZ) z[j] = Math.min(maxZ - j + 1, z[j - maxZidx]);
-            while (j + z[j] < m && s[m - 1 - z[j]] == s[m - 1 - (j + z[j])]) z[j]++;
+            if (j <= maxZ){
+                z[j] = Math.min(maxZ - j + 1, z[j - maxZidx]);
+            }
+            while (j + z[j] < m && s[m - 1 - z[j]] == s[m - 1 - (j + z[j])]){
+                ++z[j];
+            }
             if (j + z[j] - 1 > maxZ) {
                 maxZidx = j;
                 maxZ = j + z[j] - 1;
             }
         }
-        for (int j = m - 1; j > 0; j--) suffshift[m - z[j]] = j;
+        for (int j = m - 1; j > 0; j--){
+            suffshift[m - z[j]] = j;
+        }
         for (int j = 1, r = 0; j <= m - 1; j++) {
             if (j + z[j] == m) {
                 for (; r <= j; r++) {
-                    if (suffshift[r] == m) suffshift[r] = j;
+                    if (suffshift[r] == m){
+                        suffshift[r] = j;
+                    }
                 }
             }
         }
 
-        int j, bound = 0;
+        int j;
+        int bound = 0;
         int i = 0;
 
         var cbuf = new char[m];
@@ -52,7 +63,9 @@ public class SubstringSearch {
 
         while (true) {
             rdr.mark(2 * m);
-            if (rdr.read(cbuf, 0, m) != m) break;
+            if (rdr.read(cbuf, 0, m) != m){
+                break;
+            }
             for (j = m - 1; j >= bound && s[j] == cbuf[j]; ) j--;
             if (j < bound) {
                 ret.add(i);
@@ -62,7 +75,9 @@ public class SubstringSearch {
                 bound = 0;
             }
             rdr.reset();
-            if (rdr.skip(suffshift[j + 1]) != suffshift[j + 1]) break;
+            if (rdr.skip(suffshift[j + 1]) != suffshift[j + 1]){
+                break;
+            }
             i += suffshift[j + 1];
         }
 
