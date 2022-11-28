785c345c05e11c94f749e7c38f750bab838c9d62
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-11-28 19:03:07 +0700

    Update SubstringSearch.java

--- Task_1_3_1/src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
@@ -30,10 +30,10 @@ public class SubstringSearch {
         int maxZidx = 0;
         int maxZ = 0;
         for (int j = 1; j < m; j++) {
-            if (j <= maxZ){
+            if (j <= maxZ) {
                 z[j] = Math.min(maxZ - j + 1, z[j - maxZidx]);
             }
-            while (j + z[j] < m && s[m - 1 - z[j]] == s[m - 1 - (j + z[j])]){
+            while (j + z[j] < m && s[m - 1 - z[j]] == s[m - 1 - (j + z[j])]) {
                 ++z[j];
             }
             if (j + z[j] - 1 > maxZ) {
@@ -41,13 +41,13 @@ public class SubstringSearch {
                 maxZ = j + z[j] - 1;
             }
         }
-        for (int j = m - 1; j > 0; j--){
+        for (int j = m - 1; j > 0; j--) {
             suffshift[m - z[j]] = j;
         }
         for (int j = 1, r = 0; j <= m - 1; j++) {
             if (j + z[j] == m) {
                 for (; r <= j; r++) {
-                    if (suffshift[r] == m){
+                    if (suffshift[r] == m) {
                         suffshift[r] = j;
                     }
                 }
@@ -63,10 +63,12 @@ public class SubstringSearch {
 
         while (true) {
             rdr.mark(2 * m);
-            if (rdr.read(cbuf, 0, m) != m){
+            if (rdr.read(cbuf, 0, m) != m) {
                 break;
             }
-            for (j = m - 1; j >= bound && s[j] == cbuf[j]; ) j--;
+            for (j = m - 1; j >= bound && s[j] == cbuf[j]; ) {
+                j--;
+            }
             if (j < bound) {
                 ret.add(i);
                 bound = m - suffshift[0];
@@ -75,7 +77,7 @@ public class SubstringSearch {
                 bound = 0;
             }
             rdr.reset();
-            if (rdr.skip(suffshift[j + 1]) != suffshift[j + 1]){
+            if (rdr.skip(suffshift[j + 1]) != suffshift[j + 1]) {
                 break;
             }
             i += suffshift[j + 1];
