ac3e5c8f1d193a8b67f83dc57eb14ddbeabf73a4
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-12-18 15:37:02 +0700

    Update GradeBook.java

--- Task_1_4_1/src/main/java/ru/nsu/fit/oop/lab4/GradeBook.java
+++ Task_1_4_1/src/main/java/ru/nsu/fit/oop/lab4/GradeBook.java
@@ -1,5 +1,9 @@
 package ru.nsu.fit.oop.lab4;
 
+
+/**
+ * GradeBook class.
+ */
 public class GradeBook {
     private final String studentName;
     private final int countOfSemesters = 8;
@@ -11,95 +15,109 @@ public class GradeBook {
         this.courses = courses;
     }
 
-    public String getStudentName(){
+    public String getStudentName() {
         return studentName;
     }
 
-    public Course[] getCourses(){
+    public Course[] getCourses() {
         return courses;
     }
 
 
     /**
-     * count the average mark for each semester
+     * Count the average mark for each semester.
+     *
      * @return semesterAverage
      */
-    private double semesterAverage(int currentSemesters){
+    private double semesterAverage(int currentSemesters) {
         double[] averageArray = new double[countOfSemesters];
         int[] amount = new int[countOfSemesters];
         int[] sum = new int[countOfSemesters];
-        for(int i = 0 ; i < courses.length; ++i)
-        {
-            for(int j = 0 ; j < courses[i].getMarks().length; ++j)
-            {
+        for (int i = 0; i < courses.length; ++i) {
+            for (int j = 0; j < courses[i].getMarks().length; ++j) {
                 ++amount[courses[i].getMarks()[j].getSemester()];
                 sum[courses[i].getMarks()[j].getSemester()] += courses[i].getMarks()[j].getMark();
             }
         }
-        for(int i = 0; i < countOfSemesters; ++i) {
-            if(amount[i] == 0)
+        for (int i = 0; i < countOfSemesters; ++i) {
+            if (amount[i] == 0) {
                 averageArray[i] = 0;
-            else
+            }
+            else {
                 averageArray[i] = (double) sum[i] / (double) amount[i];
+            }
         }
         return averageArray[currentSemesters];
     }
 
     /**
+     * Find average mark for all semester.
+     *
      * @return the average mark for all the semesters student completed
      */
     public double averageMark()
     {
         double answer = 0;
         int amount = 0;
-        for(int i = 0; i < countOfSemesters; ++i) {
+        for (int i = 0; i < countOfSemesters; ++i) {
             answer += semesterAverage(i);
-            if(semesterAverage(i)>0)
+            if (semesterAverage(i) > 0) {
                 ++amount;
+            }
         }
-        return answer/(double) amount;
+        return answer / (double) amount;
     }
 
 
     /**
-     * checks whether the student can have a red diploma
-     * @return
+     * Checks whether the student can have a red diploma.
+     *
+     * @return true if students can have
+     *         false otherwise
      */
-    public boolean redDiploma(){
+    public boolean redDiploma() {
         int exs = 0;
         int amount = 0;
-        for(int i = 0; i < courses.length; ++i){
-            for(int j = 0 ; j < courses[i].getMarks().length; ++j)
-            {
-                if(courses[i].getMarks()[j].getMark() == 3)
+        for (int i = 0; i < courses.length; ++i) {
+            for (int j = 0; j < courses[i].getMarks().length; ++j) {
+                if (courses[i].getMarks()[j].getMark() == 3) {
                     return false;
-                if(courses[i].getCourseName() == "Дипломная работа" && courses[i].getMarks()[j].getMark() < 5)
+                }
+                if (courses[i].getCourseName().equals("Дипломная работа")
+                        && courses[i].getMarks()[j].getMark() < 5) {
                     return false;
-                if(j == courses[i].getMarks().length-1) {
-                    if (courses[i].getMarks()[j].getMark() == 5)
+                }
+                if (j == courses[i].getMarks().length - 1) {
+                    if (courses[i].getMarks()[j].getMark() == 5) {
                         ++exs;
+                    }
                     ++amount;
                 }
             }
         }
         double average = (double) exs / (double) amount;
-        if (average > 0.75)
+        if (average > 0.75) {
             return true;
+        }
         return false;
     }
 
     /**
-     * checks whether the student will have a higher scholarship in the selected semester
-     * considering the results of last session
-     * @param selectedSemester
-     * @return
+     * Checks whether the student will have a higher scholarship in the selected semester
+     * considering the results of last session.
+     *
+     * @param selectedSemester semester we need to know about higher scholarship
+     *
+     * @return true if can
+     *         false otherwise
      */
-    public boolean higherScholarship(int selectedSemester)
-    {
-        if(selectedSemester == 0)
+    public boolean higherScholarship(int selectedSemester) {
+        if (selectedSemester == 0) {
             return false;
-        if(semesterAverage(selectedSemester-1) != 5)
+        }
+        if (semesterAverage(selectedSemester - 1) != 5) {
             return false;
+        }
         return true;
     }
 }
