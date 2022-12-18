e737c2998d0d16c5cf934973d8eff516f2defb66
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-12-18 15:37:30 +0700

    Update GradeBookTests.java

--- Task_1_4_1/src/test/java/ru/nsu/fit/oop/lab4/GradeBookTests.java
+++ Task_1_4_1/src/test/java/ru/nsu/fit/oop/lab4/GradeBookTests.java
@@ -1,53 +1,60 @@
 package ru.nsu.fit.oop.lab4;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
+/**
+ * Gradebook test class.
+ */
 public class GradeBookTests {
-    Marks[] CreateMarks(int[][] marks){
+    Marks[] createmarks(int[][] marks) {
         Marks[] mks = new Marks[marks.length];
-        for(int i = 0; i < marks.length; ++i) {
-            mks[i] = new Marks(0,0);
+        for (int i = 0; i < marks.length; ++i) {
+            mks[i] = new Marks(0, 0);
             mks[i].setMark(marks[i][0], marks[i][1]);
         }
         return mks;
     }
 
-    GradeBook CreateGradeBook(String name, int[][][] marks){
-        String[] lessons = {
-                "matan", "matlog", "english", "pe", "platforms", "imperative", "declarative", "history", "russian"
+    GradeBook creategradebook(String name, int[][][] marks) {
+        String[] lessons = {"matan", "matlog", "english", "pe",
+            "platforms", "imperative", "declarative", "history", "russian"
         };
         Course[] coursesAndMarks = new Course[lessons.length];
-        for(int i = 0; i < lessons.length; ++i){
-            coursesAndMarks[i] = new Course(lessons[i], CreateMarks(marks[i]));
+        for (int i = 0; i < lessons.length; ++i) {
+            coursesAndMarks[i] = new Course(lessons[i], createmarks(marks[i]));
         }
         return new GradeBook(name, coursesAndMarks);
     }
 
+    /**
+     * All tests for this task
+     *
+     * @param name student name
+     * @param marks stident marks
+     */
     @ParameterizedTest
     @MethodSource("allTests")
-    public void GradeBookTests(String name, int[][][] marks) {
-        GradeBook gb = CreateGradeBook(name, marks);
-        if(name == "Timur Ilinykh"){
+    public void allgradebooktests(String name, int[][][] marks) {
+        GradeBook gb = creategradebook(name, marks);
+        if (name.equals("Timur Ilinykh")) {
             assertFalse(gb.redDiploma());
             assertFalse(gb.higherScholarship(0));
             assertFalse(gb.higherScholarship(1));
             assertTrue(gb.averageMark() > 4.6);
         }
-        if(name == "lazyPerson"){
+        if (name.equals("lazyPerson")) {
             assertFalse(gb.redDiploma());
             assertFalse(gb.higherScholarship(0));
             assertFalse(gb.higherScholarship(1));
             assertTrue(gb.averageMark() == 3.0);
         }
-        if(name == "tryHardPerson"){
+        if (name.equals("tryHardPerson")) {
             assertTrue(gb.redDiploma());
             assertFalse(gb.higherScholarship(0));
             assertTrue(gb.higherScholarship(1));
@@ -58,37 +65,37 @@ public class GradeBookTests {
     private static Stream<Arguments> allTests() {
         return Stream.of(
                 Arguments.of("Timur Ilinykh", new int[][][]{
-                        {{0,5},{1,4}},
-                        {{0,5},{1,4}},
-                        {{0,5},{1,4}},
-                        {{0,5},{1,5}},
-                        {{0,5},{1,5}},
-                        {{0,4},{1,4}},
-                        {{0,5},{1,5}},
-                        {{0,5}},
-                        {{0,5}}
+                        {{0, 5}, {1, 4}},
+                        {{0, 5}, {1, 4}},
+                        {{0, 5}, {1, 4}},
+                        {{0, 5}, {1, 5}},
+                        {{0, 5}, {1, 5}},
+                        {{0, 4}, {1, 4}},
+                        {{0, 5}, {1, 5}},
+                        {{0, 5}},
+                        {{0, 5}}
                 }),
                 Arguments.of("lazyPerson", new int[][][]{
-                        {{0,3},{1,3}},
-                        {{0,3},{1,3}},
-                        {{0,3},{1,3}},
-                        {{0,3},{1,3}},
-                        {{0,3},{1,3}},
-                        {{0,3},{1,3}},
-                        {{0,3},{1,3}},
-                        {{0,3}},
-                        {{0,3}}
+                        {{0, 3}, {1, 3}},
+                        {{0, 3}, {1, 3}},
+                        {{0, 3}, {1, 3}},
+                        {{0, 3}, {1, 3}},
+                        {{0, 3}, {1, 3}},
+                        {{0, 3}, {1, 3}},
+                        {{0, 3}, {1, 3}},
+                        {{0, 3}},
+                        {{0, 3}}
                 }),
                 Arguments.of("tryHardPerson", new int[][][]{
-                        {{0,5},{1,5}},
-                        {{0,5},{1,5}},
-                        {{0,5},{1,5}},
-                        {{0,5},{1,5}},
-                        {{0,5},{1,5}},
-                        {{0,5},{1,5}},
-                        {{0,5},{1,5}},
-                        {{0,5}},
-                        {{0,5}}
+                        {{0, 5}, {1, 5}},
+                        {{0, 5}, {1, 5}},
+                        {{0, 5}, {1, 5}},
+                        {{0, 5}, {1, 5}},
+                        {{0, 5}, {1, 5}},
+                        {{0, 5}, {1, 5}},
+                        {{0, 5}, {1, 5}},
+                        {{0, 5}},
+                        {{0, 5}}
                 })
         );
     }
