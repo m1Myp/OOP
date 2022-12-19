8ec64de14d5c6b81c3ae9cf5f03c7727e17359cf
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-12-19 10:36:39 +0700

    Update Notebook.java

--- Task_1_5_2/src/main/java/ru/nsu/fit/oop/lab5/Notebook.java
+++ Task_1_5_2/src/main/java/ru/nsu/fit/oop/lab5/Notebook.java
@@ -1,6 +1,11 @@
 package ru.nsu.fit.oop.lab5;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.Reader;
