dbcf220545d35fe7eb10e5dbe8b32a8f0f672e71
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-12-19 10:10:17 +0700

    Add files via upload

---
+++ Task_1_5_2/src/test/java/ru/nsu/fit/oop/lab5/NotebookCommandLineTest.java
@@ -0,0 +1,36 @@
+package ru.nsu.fit.oop.lab5;
+
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NotebookCLITest {
+    @Test
+    void commandline() {
+        Notebook nb = new Notebook();
+        NotebookCommandLine commandLine = new NotebookCommandLine(nb);
+        CommandLine executor = new CommandLine(commandLine)
+                .registerConverter(Date.class, s -> new SimpleDateFormat("dd.MM.yyyy HH:mm").parse(s));
+        executor.execute("add", "First m", "textbewr5trebtt5");
+        executor.execute("add", "Bad", "textbrebttewthyw5");
+        executor.execute("add", "Second m", "textbrebtt5e5h4");
+        executor.execute("add", "Third k", "textbrebtt5erh5r6");
+        executor.execute("rm", "Bad");
+
+        assertThat(nb.getNotes().size()).isEqualTo(3);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(stream));
+        executor.execute("show");
+        assertThat(stream.toString()).contains("First");
+        assertThat(stream.toString()).contains("Second");
+        assertThat(stream.toString()).contains("Third");
+    }
+}
