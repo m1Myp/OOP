0caa35aa53a396738d9974f4e3afe94e4877911c
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-12-23 19:17:13 +0700

    Add files via upload

---
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/lab6/Calculator.java
@@ -0,0 +1,93 @@
+package ru.nsu.fit.oop.lab6;
+
+import org.apache.commons.math3.complex.Complex;
+
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
+
+record Operation(String regex, int arity, BiFunction<Complex[], String, Complex> evaluate) {
+        Operation(String regex, int arity, Function<Complex[], Complex> f){
+        this(regex, arity, (values, s) -> f.apply(values));
+        }
+        }
+
+record Token(Operation op, String text) {
+        }
+
+public class Calculator {
+    private final List<Operation> operations = new ArrayList<>();
+
+    static class UnknownTokenException extends RuntimeException{
+        public final String tokenText;
+        UnknownTokenException(String message, String tokenText){
+            super(message);
+            this.tokenText = tokenText;
+        }
+    }
+
+    static class InvalidArityException extends RuntimeException{}
+
+    Calculator() {
+        operations.add(new Operation("\\+", 2, (values) -> values[0].add(values[1])));
+        operations.add(new Operation("-", 2, (values) -> values[0].subtract(values[1])));
+        operations.add(new Operation("\\*", 2, (values) -> values[0].multiply(values[1])));
+        operations.add(new Operation("/", 2, (values) -> values[0].divide(values[1])));
+        operations.add(new Operation("pow", 2, (values) -> values[0].pow(values[1])));
+        operations.add(new Operation("cos", 1, (values) -> values[0].cos()));
+        operations.add(new Operation("sin", 1, (values) -> values[0].sin()));
+        operations.add(new Operation("log", 1, (values) -> values[0].log()));
+        operations.add(new Operation("sqrt", 1, (values) -> values[0].sqrt()));
+        operations.add(new Operation("-?\\d+(\\.\\d+)?", 0, (values, s) -> new Complex(Double.parseDouble(s))));
+        operations.add(new Operation("PI", 0, (values, s) -> new Complex(Math.PI)));
+        operations.add(new Operation("e", 0, (values, s) -> new Complex(Math.E)));
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print('>');
+            String str = sc.nextLine();
+            if (str.equals(".quit")) break;
+            try {
+                Complex res = calc.evaluateString(str);
+                System.out.println(res.getReal());
+            } catch (UnknownTokenException e) {
+                System.out.println("ERROR: " + e.getMessage());
+            }catch (InvalidArityException e){
+                System.out.println("Invalid number of arguments");
+            }
+        }
+    }
+
+    public Complex evaluateString(String str) {
+        Stack<Complex> stack = new Stack<>();
+        Object[] wtf = Arrays.stream(str.split("\\s+")).map((s) -> {
+            for (Operation op : operations) {
+                if (Pattern.matches("^" + op.regex() + "$", s)) {
+                    return new Token(op, s);
+                }
+            }
+            throw new UnknownTokenException("Unknown token: " + s, s);
+        }).toArray();
+        Token[] tokens = Arrays.copyOf(wtf, wtf.length, Token[].class);
+
+
+        for (var i = tokens.length - 1; i >= 0; i--) {
+            Token token = tokens[i];
+            Operation op = token.op();
+            if (stack.size() < op.arity()) throw new InvalidArityException();
+            Complex[] args = new Complex[op.arity()];
+            for (var j = 0; j < op.arity(); j++) {
+                args[j] = stack.pop();
+            }
+            stack.push(op.evaluate().apply(args, token.text()));
+        }
+
+        if (stack.size() != 1) throw new InvalidArityException();
+        return stack.pop();
+    }
+}
---
+++ Task_1_5_1/src/test/java/ru/nsu/fit/oop/lab6/CalculatorTest.java
@@ -0,0 +1,50 @@
+package ru.nsu.fit.oop.lab6;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTests {
+    @ParameterizedTest(name = "[{index}] {0} == {1}")
+    @MethodSource("getExpressions")
+    void evaluateString(String expression, double result) {
+        Calculator calc = new Calculator();
+        assertEquals(result, calc.evaluateString(expression).getReal(), 0.01d);
+    }
+
+    @Test
+    @DisplayName("Test invalid token")
+    void evaluateString_InvalidToken(){
+        assertThrows(Calculator.UnknownTokenException.class, ()
+                -> new Calculator().evaluateString("Is It String? Isnt It?"));
+    }
+
+    @Test
+    @DisplayName("Test invalid arity")
+    void evaluateString_InvalidArity(){
+        assertThrows(Calculator.InvalidArityException.class, ()
+                -> new Calculator().evaluateString("+ 1"));
+    }
+
+    static Stream<Arguments> getExpressions(){
+        return Stream.of(
+                Arguments.of("+ 1 1", 2d),
+                Arguments.of("* + 2 3 5", 25d),
+                Arguments.of("- * + 1 5 / 5 2 1", 14d),
+                Arguments.of("cos 0", 1d),
+                Arguments.of("sin 0", 0d),
+                Arguments.of("pow 2 10", 1024d),
+                Arguments.of("sqrt 9", "3"),
+                Arguments.of("log e", 1d),
+                Arguments.of("sin / PI 2", 1d)
+        );
+    }
+
+}
