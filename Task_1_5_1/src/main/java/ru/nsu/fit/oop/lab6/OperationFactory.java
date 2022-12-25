615e0f4c95c44ca45811b07c9e73324b7fef44eb
m1Myp <55120919+m1Myp@users.noreply.github.com>
2022-12-25 21:30:26 +0700

    Add files via upload

--- Task_1_5_1/src/main/java/ru/nsu/fit/oop/lab6/OperationFactory.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/lab6/OperationFactory.java
@@ -2,7 +2,19 @@ package ru.nsu.fit.oop.lab6;
 
 import java.util.regex.Pattern;
 
+/**
+ * Operation factory class.
+ */
 public class OperationFactory {
+    /**
+     * Checking that our string have operation from op list.
+     *
+     * @param op operation from what we need regex
+     * @param s string from what we take op
+     *
+     * @return Token we need
+     *          Token(null, null) if it is not an operation
+     */
     public Token getOp(Operation op, String s) {
         if (Pattern.matches("^" + op.regex() + "$", s)) {
             return new Token(op, null);
