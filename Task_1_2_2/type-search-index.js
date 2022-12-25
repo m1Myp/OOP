caa4a6533898230fa0443458be453d19b1092dd6
m1Myp <m1Myp@users.noreply.github.com>
2022-12-25 13:44:58 +0000

    Deploying to gh-pages from @ m1Myp/OOP@1c27ab2247cce5eb11d9c9dda8685a8142a71a22 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.fit.oop.lab3","l":"Tree.BreadthFirstSearch"},{"p":"ru.nsu.fit.oop.lab3","l":"Tree.DepthFirstSearch"},{"p":"ru.nsu.fit.oop.lab3","l":"Tree"}]
